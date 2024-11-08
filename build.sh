a147bc57825e9b9ea8aada5a0923ae7cf1401c7c
julia <me@juliapaci.com>
2024-11-08 16:16:51 +1100

    linking with stb_image

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-nasm -f elf64 -F stabs program.s
-ld -o program program.o
+printf "#define STB_IMAGE_IMPLEMENTATION\n#include \"stb_image.h\"" > stb_image_wrapper.c
+gcc -o stb_image_wrapper.o -c stb_image_wrapper.c -no-pie
+nasm -f elf64 program.S
+ld -o program program.o stb_image_wrapper.o -lc -lm -dynamic-linker /lib64/ld-linux-x86-64.so.2
