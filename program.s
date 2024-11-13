bc2d148838c18c43c3ee7b3d981d554da28c4d4a
julia <me@juliapaci.com>
2024-11-13 15:59:32 +1100

    command line arguments

--- program.s
+++ program.s
@@ -2,27 +2,36 @@ section .text
     default rel
     global _start
 
+; moves argc to %rdi and argv to %rsi
+load_arguments:
+    ; offset by 16 because of call return address and alignment
+    mov rdi, [rsp + 16]          ; argc
+    lea rsi, [rsp + 24]      ; argv[0]
+
+    ret
+
 _start:
     push rbp
     mov rbp, rsp
 
-    ; [extern stbi_load]
-    ; cmp dword [rbp - 4], 1
-    ; je .end
+    call load_arguments
 
-    [extern printf]
+    ; exits if no image/options was supplied
+    cmp rdi, 1
+    je .end
+
+    extern printf
+    mov rsi, rdi
     mov rdi, fmt
-    mov rsi, str
     mov rax, 0
     call printf wrt ..plt
 
     pop rbp
+
 .end:
     mov rax, 60 ; exit syscall
     mov rdi, 0  ; EXIT_SUCCESS
     syscall
 
 section .data
-    str: db "Hello, World", 0Ah, 0
-    str_len: equ $ - str
-    fmt: db "%s", 0Ah, 0
+    fmt: db "%d", 0Ah, 0
