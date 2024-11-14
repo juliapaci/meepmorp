6b63e26fb6a8e9cb8aa724f644fab4632ce1917f
julia <me@juliapaci.com>
2024-11-14 16:26:44 +1100

    loading pixel data
    wip, still need an easily mutable buffer

--- program.s
+++ program.s
@@ -2,12 +2,37 @@ section .text
     default rel
     global _start
 
-; moves argc to %rdi and argv to %rsi
+; moves argc to %rax and argv to %rbx
 load_arguments:
     ; offset by 16 because of call return address and alignment
-    mov rdi, [rsp + 16]          ; argc
-    lea rsi, [rsp + 24]      ; argv[0]
+    mov rax, [rsp + 16] ; argc
+    lea rbx, [rsp + 24] ; argv[0]
 
+    ret
+
+; loads image pixel data into a buffer
+; expects file name in rdi
+get_image_data:
+    push rbp
+    mov rbp, rsp
+
+    lea rax, [rdi]
+
+    ; load image data
+    extern stbi_load    ; "stb_image.h"
+    lea rdi, [rax]      ; filename = argv[0]
+    lea rsi, [rsp]      ; [ *x  ]   <- rsp
+    lea rdx, [rsp + 64] ; [ *y  ]   <- rsp + sizeof(size_t)
+    lea rcx, [rsp + 128]; [*comp]   <- rsp + 2*sizeof(size_t)
+    xor r8, r8          ; req_comp = 0
+    call stbi_load      ; rax = pixel byte buffer
+    ; TODO: check stb errors
+
+    ; mov rax, 9          ; mmap
+    ; xor rdi, rdi        ; addr = NULL
+    ; moc rsi,
+
+    pop rbp
     ret
 
 _start:
@@ -20,17 +45,23 @@ _start:
     cmp rdi, 1
     je .end
 
+    push rbx
+
+    ; print argc
     extern printf
-    mov rsi, rdi
     mov rdi, fmt
-    mov rax, 0
+    mov rsi, rax
+    xor rax, rax
     call printf wrt ..plt
+
+    pop rdi         ; save argv[0] (image file name)
+    call get_image_data
 
     pop rbp
 
 .end:
-    mov rax, 60 ; exit syscall
-    mov rdi, 0  ; EXIT_SUCCESS
+    mov rax, 60     ; exit syscall
+    xor rdi, rdi    ; EXIT_SUCCESS
     syscall
 
 section .data
